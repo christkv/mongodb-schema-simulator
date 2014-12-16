079a21a653dc0ccdd817f5a2eb034f063ff0509e
Christian Kvalheim <christkv@gmail.com>
2014-12-16 14:47:19 +0100

    Added pre-allocation test for embedded, and remove tracing

--- chapters/chapter_2/embedded.js
+++ chapters/chapter_2/embedded.js
@@ -14,6 +14,46 @@ var Embedded = function(module, args) {
 // Inherit from Case
 inherits(Embedded, Case);
 
+// Pre-allocated array
+var _id = new ObjectId();
+// Create pre-allocated array
+var _b = [];  
+for(var i = 0; i < 1000; i++) {
+  _b.push(_id)
+}
+
+/*
+ * Pre-allocate second boxes for the total amount of entries
+ */
+var preAllocate = function(self, callback) {
+  // Total number of preallocations
+  self.totalPreallocations = self.args.p * self.args.r;
+  var counter = 0;
+  var left = self.totalPreallocations;
+  // Set the timestamp
+  var timestamp = new Date();
+  timestamp.setMilliseconds(0);
+  timestamp.setSeconds(counter);
+
+  // Insert the pre-allocated documents
+  for(var i = 0; i < self.totalPreallocations; i++) {
+    // Just using a dummy counter as the timestamp here
+    self.collection.insert({
+      _id: "" + process.pid + "_" + counter++,
+      b: _b
+    }, function(err) {
+      left = left - 1;
+
+      if(left == 0) {
+        callback();
+      }
+    });
+
+    // Set the seconds
+    timestamp.setSeconds(timestamp.getSeconds() + 1);
+  }
+}
+
 /*
  * Setup and tear down methods for the embedded class
  */
@@ -25,9 +65,16 @@ Embedded.prototype.setup = function(options, callback) {
   this.connect(function(err, db) {
     if(err) return callback(err);
     // Set our collection
-    self.collection = db.collection(f('embedded_%s', process.pid));
-    // Return after setup
-    callback();
+    self.collection = db.collection(f('embedded'));
+
+    // Drop the collection
+    self.collection.drop(function() {
+      // Return if trying with no index
+      if(self.args.m == 'pre_allocated') 
+        return preAllocate(self, callback)
+
+      callback();
+    });
   });
 }
 
@@ -70,7 +117,28 @@ Embedded.prototype.embedded = function(options, callback) {
  */
 Embedded.prototype.embeddedRandom = function(options, callback) {  
   if(typeof options == 'function') callback = options, options = {};
-  callback();
+  var id = new ObjectId();
+
+  // Number of entries to push into the document
+  var number = Math.round(Math.random() * 1000) + 1;
+  var self = this;
+
+  // Process all the updates
+  var pushToDoc = function(c, id, collection, callback) {
+    if(c == 0) return callback();
+
+    collection.updateOne({_id: id}, {$push: {b: {c: id}}}, {w:1}, function(err, r) {
+      if(err) return callback(err);
+      pushToDoc(c - 1, id, collection, callback);
+    });
+  }
+
+  // Insert an empty document
+  var doc = {_id: id, a:1, b: []};
+  this.collection.insert(doc, {w:1}, function(err, r) {
+    if(err) return callback(err);
+    pushToDoc(number, id, self.collection, callback);
+  });
 }
 
 /*
@@ -78,7 +146,34 @@ Embedded.prototype.embeddedRandom = function(options, callback) {
  */
 Embedded.prototype.preAllocated = function(options, callback) {  
   if(typeof options == 'function') callback = options, options = {};
-  callback();
+  var id = new ObjectId();
+  var self = this;
+
+  // Number of entries to push into the document
+  var number = 1000
+  var self = this;
+
+  // Process all the updates
+  var pushToDoc = function(c, id, collection, callback) {
+    if(c == 0) return callback();
+
+    collection.updateOne({_id: "" + process.pid + "_" + c}, {$push: {b: id}}, {w:1}, function(err, r) {
+      if(err) return callback(err);
+      pushToDoc(c - 1, id, collection, callback);
+    });
+  }
+
+  // Insert an empty document
+  var doc = {_id: id, a:1, b: _b};
+  this.collection.insert(doc, {w:1}, function(err, r) {
+    if(err) return callback(err);
+
+    // Set empty array (in effect pre-allocating array space)
+    self.collection.updateOne({_id: id}, {$set: {b: []}}, {w:1}, function(err, r) {
+      if(err) return callback(err);
+      pushToDoc(number, id, self.collection, callback);
+    });
+  });
 }
 
 // Export schema
@@ -94,12 +189,12 @@ module.exports = {
     , method: 'embedded'
     , description: 'Growing embedded document array to fixed 1000 entries'
   }, {
-      name: 'embedded-random'
+      name: 'embedded_random'
     , method: 'embeddedRandom'
     , description: 'Growing embedded document array to random sized entries max of 1000'
   }, {
-      name: 'pre-allocated'
+      name: 'pre_allocated'
     , method: 'preAllocated'
-    , description: "Adding to pre-allocated embedded document array random sized entries max of 1000"
+    , description: "Adding 1000 items to pre-allocated embedded document array"
   }]  
 }
