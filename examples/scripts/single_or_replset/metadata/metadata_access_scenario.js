f294cecc11823203cf9d1680a9d7c8c4f2eabaee
Christian Kvalheim <christkv@gmail.com>
2015-04-01 14:44:29 +0200

    Allowed for read preferences for metadata schema

--- examples/scripts/single_or_replset/metadata/metadata_access_scenario.js
+++ examples/scripts/single_or_replset/metadata/metadata_access_scenario.js
@@ -19,7 +19,7 @@ module.exports = [{
   // readPreference settings
   readPreferences: {
     metadata: {
-        readPreference: 'secondaryPreferred'
+        mode: 'secondaryPreferred'
       , tags: {}
     }
   },
@@ -40,8 +40,8 @@ module.exports = [{
   // Execution plan is run using all the process.openStdin();
   execution: {
     // Number of ticks/iterations we are running
-      iterations: 25
+      iterations: 100
     // Number of users starting the op at every tick
-    , numberOfUsers: 50
+    , numberOfUsers: 350
   }
 }];
